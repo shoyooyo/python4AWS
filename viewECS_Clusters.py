ff36425df16fbcc149ff691420aba4981c6a5403
ajaz <maahmed@ciphercloud.com>
2020-02-14 12:48:29 +0530

    version 2 - using response as a dictionary directly

--- viewECS_Clusters.py
+++ viewECS_Clusters.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 import boto3
+import json
 
 client = boto3.client('ecs')
 i = 0
 
-for i in range(0,1):
+'''for i in range(0,1):
     response = client.list_clusters()
-    print(response )
+    print(response )'''
 
 
-'''response = client.list_clusters(
+response = client.list_clusters(
     maxResults=50
 )
-print(response)'''
+y=response["clusterArns"]
+print(y)
 
 
